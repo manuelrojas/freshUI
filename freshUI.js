f9923f123825c6ad320ab99f711ff9ff4b8470e0
manuelrojas <manuel.rojas.21@gmail.com>
2013-02-18 10:31:46 -0600

    Test version

--- freshUI.js
+++ freshUI.js
@@ -1,18 +1,32 @@
+/* ==========================================================
+ * freshUI.js v0.0.1
+ * http://www.mrojasweb.com/
+ * ==========================================================
+ * Author:Manuel Rojas
+ * Date:2/12/2013
+ * Description: Plugin to show a banner with descriptions and
+ * images.
+ * ========================================================== */
+
 (function ($,window) {
 	$.fn.freshUI = function(options){
-		
-		$(this).children("p").each(function(){
-			$(this).animate({
-					      		bottom: "100px", 
-					      		opacity: 1
-					    	}, 
-					    	{ 
-					    		duration: 500, 
-					    		queue: false 
-					    	});
-			console.log("each");
-		});
-
-		
+		freshUI.init(options);
 	}
+	
+	var freshUI = {
+
+		init: function(options){
+			/*this.opts = options;*/
+			this.animateDescriptions();
+		},
+		animateDescriptions: function(){
+			$('.banner > p').each(function(index){
+				//Animate each element
+				$(this)
+				.delay(1000*index)
+				.animate({bottom: "+=100px", opacity: 1}, "slow");
+				
+			});
+		}
+	};
 })(jQuery,window)
